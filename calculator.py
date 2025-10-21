542d81f699efaa08497e2c8b50c4ea06bc653d24
zjh <3608034157@qq.com>
2025-10-21 14:54:52 +0800

    again

--- calculator.py
+++ calculator.py
@@ -6,4 +6,4 @@ def add(a, b):
     """
     if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
         raise TypeError("Both arguments must be numbers")
-    return a + b
+    return a + b + 1
